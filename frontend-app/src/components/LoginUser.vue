20730c4a96dd4574cef8b6c7e1621f907488cb29
github Na0Se <minminnminm333@gmail.com>
2023-07-23 12:14:03 +0900

    done register

--- frontend-app/src/components/LoginUser.vue
+++ frontend-app/src/components/LoginUser.vue
@@ -1,47 +1,100 @@
 <template>
-    <div>
-        <h1>体調管理アプリ</h1>
-
-        <form>
-        <input type="text" placeholder="username" required v-model="inputUser.username"/><br>
-        <input type="password" placeholder="password" required v-model="inputUser.password"/><br>
-        <button @click="login">LOGIN</button>
-        </form>
-    </div>
+    <div class="app-container">
+    <h1>体調管理アプリ</h1>
+    <form class="login-form">
+      <label for="username">ユーザーネーム：</label>
+      <input type="text" id="username" placeholder="username" required v-model="inputUser.username" /><br>
+      <label for="password">パスワード：</label>
+      <input type="password" id="password" placeholder="password" required v-model="inputUser.password" /><br>
+      <button @click="login">LOGIN</button>
+    </form>
+    <router-link class="register-link" to="/register">初めての人はこちらから新規登録</router-link>
+  </div>
 </template>
 
 <script>
-import {inject} from 'vue';
+import { inject } from 'vue';
 import { useRouter } from 'vue-router';
 
 export default {
     name: 'LoginUser',
-    setup(){
+    setup() {
         const inputUser = {
             username: '',
             password: ''
         }
 
-        const {loginUser} = inject('fetchStore');
+        const { loginUser } = inject('fetchStore');
 
         const router = useRouter();
 
         const login = async (event) => {
             event.preventDefault();
-            if(inputUser.username&&inputUser.password){
-                try{
+            if (inputUser.username && inputUser.password) {
+                try {
                     await loginUser(inputUser);
                     router.push('/condition');
-                } catch(error){
+                } catch (error) {
                     window.alert('ユーザーネームかパスワードが間違ってるみたいだよ');
                 }
             }
         };
-        
-        return{
+
+        return {
             inputUser,
             login
         }
     }
 }
 </script>
+
+<style scoped>
+.app-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.login-form label {
+  font-weight: bold;
+}
+
+.login-form input {
+  width: 100%;
+  padding: 10px;
+  margin: 5px 0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+.register-link {
+  display: block;
+  margin-top: 20px;
+  color: #007bff;
+  text-decoration: none;
+}
+</style>
--- frontend-app/src/components/RegisterUser.vue
+++ frontend-app/src/components/RegisterUser.vue
@@ -1,12 +1,101 @@
 <template>
+    <div class="app-container">
     <h1>体調管理アプリ</h1>
+    <form class="register-form">
+      <label for="username">ユーザーネーム：</label>
+      <input type="text" id="username" placeholder="username" required v-model="inputUser.username" /><br>
+      <label for="password">パスワード：</label>
+      <input type="password" id="password" placeholder="password" required v-model="inputUser.password" /><br>
+      <label for="morePassword">パスワード再入力：</label>
+      <input type="password" id="morePassword" placeholder="one more password" required v-model="inputUser.password_confirmation" /><br>
+      <button @click.prevent="register">REGISTER</button>
+    </form>
+    <router-link class="back-link" to="/">ログイン画面に戻る</router-link>
+  </div>
 </template>
 
 <script>
+import {inject} from 'vue';
+import { useRouter } from 'vue-router';
+
 export default {
     name: 'RegisterUser',
-    setup(){
-        
+    setup() {
+        const inputUser = {
+            username: '',
+            password: '',
+            password_confirmation: ''
+        }
+
+        const {registerUser} = inject('fetchStore');
+        const router = useRouter();
+
+        const register = async () => {
+            if (inputUser.username && inputUser.password && inputUser.password_confirmation) {
+                try{
+                    await registerUser(inputUser);
+                    router.push('/condition');
+                } catch(error){
+                    console.log(error);
+                }
+            }
+        };
+
+        return {
+            inputUser,
+            register
+        }
     }
 }
 </script>
+
+<style scoped>
+.app-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+
+.register-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.register-form label {
+  font-weight: bold;
+}
+
+.register-form input {
+  width: 100%;
+  padding: 10px;
+  margin: 5px 0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+.back-link {
+  display: block;
+  margin-top: 20px;
+  color: #007bff;
+  text-decoration: none;
+}
+</style>
